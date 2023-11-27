76b54fbd347a3ab4d3520f58011c2698395d70bf
Kareem <kareemelnaghy@aucegypt.edu>
2023-11-27 20:46:26 +0200

    Created new header and source file. Adjusted functions belonging to problem 1.

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/mac/Desktop/Data Structures/Assignment-5-ADS/cmake-build-debug/Problem 1.cpp" "CMakeFiles/untitled.dir/Problem_1.cpp.o" "gcc" "CMakeFiles/untitled.dir/Problem_1.cpp.o.d"
+  "/Users/mac/Desktop/Data Structures/Assignment-5-ADS/cmake-build-debug/main.cpp" "CMakeFiles/untitled.dir/main.cpp.o" "gcc" "CMakeFiles/untitled.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
